2d6b779f09a349b12db4f79d78e0fe0faf826b36
iwasaki-hub <66316321+iwasaki-hub@users.noreply.github.com>
2022-11-28 16:44:58 +0900

    recipe app

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,19 @@
-import '../styles/globals.css'
+import "../styles/globals.css";
+import Link from "next/link";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <nav className="header">
+        <div>
+          <Link href="/">Kap's Kitchen 🍍</Link>
+        </div>
+      </nav>
+      <main>
+        <Component {...pageProps} />
+      </main>
+    </>
+  );
 }
 
-export default MyApp
+export default MyApp;
